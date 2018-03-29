fd2ef3501a278adf5c924f94f6e4a5e4f177e25e
benoit mathonat <benoit.mathonat@gmail.com>
2018-03-29 16:25:59 +0200

    route cockatil

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329130023) do
+ActiveRecord::Schema.define(version: 20180329133345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,18 @@ ActiveRecord::Schema.define(version: 20180329130023) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "doses", force: :cascade do |t|
+    t.text "description"
+    t.bigint "cocktail_id"
+    t.bigint "ingredient_id"
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+  end
+
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
